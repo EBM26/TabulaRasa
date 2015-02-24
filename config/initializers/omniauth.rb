cce7605aca0ea981d911fcd65d1781986a4650d1
Eyal Benmoshe <ebm2626@yahoo.com>
2015-02-23 19:51:56 -0800

    received calendar hash back

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 ## Google oauth code that lets people log in with google
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["Client Id"], ENV["_-_1x3HYX_TRt8l1Ou_YZ-D_"], {
+  provider :google_oauth2, ENV["Client_Id"], ENV["Client_Secret"], {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
-    redirect_uri:'http://localhost/auth/google_oauth2/callback'
+    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'
   }
 end
