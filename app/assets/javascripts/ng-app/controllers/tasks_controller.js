a34c3bca7250714074e33d97ac296a1a7c2b8dc6
Eyal Benmoshe <ebm2626@yahoo.com>
2015-02-26 16:24:21 -0800

    got show and delete to work on Tasks

--- app/assets/javascripts/ng-app/controllers/tasks_controller.js
+++ app/assets/javascripts/ng-app/controllers/tasks_controller.js
@@ -12,18 +12,45 @@ angular
   angular 
   .module("tabulaApp")
   .controller("tasksController", ["$scope", "$http", "$resource", "$state", "Task", "$modal", function ($scope, $http, $resource, $state, Task, $modal) {
-   Task.query(function(data){
-    $scope.tasks = data;
-   });
+    Task.query(function(data){
+      $scope.tasks = data;
+    });
 
-   $scope.openAddTask = function() {
-        $modal.open({
-      templateUrl: 'tasks/new.html',
-      controller: 'newTaskController'
+    $scope.deleteTask = function(task) {
+      task.$delete(function(){
+        Task.query(function(data){
+          $scope.tasks = data;
+        });
+      });
+    }
 
- });
-  }
-}]);
+    $scope.openAddTask = function() {
+      $modal.open({
+        templateUrl: 'tasks/new.html',
+        controller: 'newTaskController'
+      });
+    };
+    $scope.openShowTask = function(id) {
+      $modal.open({
+        templateUrl: 'tasks/show.html',
+        controller: 'showTasksController',
+        resolve: {
+          task_id: function () {
+            return id;
+          }
+        }
+      });
+    };
+  }]);
+
+angular
+    .module("tabulaApp")
+    .controller("showTasksController", ["$scope", "$http", "$state", "$resource", "$stateParams", "Task", "$modalInstance", "task_id", function ($scope, $http, $state, $resource, $stateParams, Task, $modalInstance, task_id){
+      $scope.task = Task.get({ id: task_id });
+
+
+    }]);
+
 
 
 angular
