21a2e8a0b4988d27e149a1a79af61f26c7d7fa23
Eyal Benmoshe <ebm2626@yahoo.com>
2015-02-27 09:57:15 -0800

    destroy list function works

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,8 +2,9 @@ class List < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  has_many :items
+  has_many :items, dependent: :destroy
 
   belongs_to :user
+
 
 end
